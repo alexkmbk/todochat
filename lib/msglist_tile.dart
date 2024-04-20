05044c85e589fe9d1ca225a83993e6a5755e83cc
alexkmbk <alexkmbkdr1@gmail.com>
2024-04-21 03:01:36 +0500

    context menu

--- lib/msglist_tile.dart
+++ lib/msglist_tile.dart
@@ -148,7 +148,7 @@ class _MsgListTileState extends State<MsgListTile> {
                 "/FileStorage/" +
                 message.parentsmallImageName
             : "",
-        headers: {"sessionID": sessionID},
+        HTTPHeaders: {"sessionID": sessionID},
         onCopy: () => Clipboard.setData(ClipboardData(text: message.text)),
         onReply: () {
           msgListProvider.quotedText = message.text;
@@ -170,192 +170,221 @@ class _MsgListTileState extends State<MsgListTile> {
           msgListProvider.refresh();
         },
       );
-    } else if (widget.message.fileName.isEmpty) {
-      final text = widget.message.isTaskDescriptionItem
-          ? widget.msgListProvider.task.description
-          : widget.message.text;
-      final textSpan = TextSpan(
-        text: text,
-        // recognizer: TapGestureRecognizer()
-        //   ..onSecondaryTapDown = (value) {
-        //     print('Tap Here onTap');
-        //   },
-      );
-      //BoolRef isQuoteSelected = BoolRef();
-      final textWidget = SelectableText.rich(
-        textSpan,
-        onSelectionChanged: (selection, cause) {
-          textWidgetSelection = selection;
-          textGestureDetectorWithMenu.isQuoteSelected = true;
-        },
-        contextMenuBuilder: null,
-
-        // contextMenuBuilder: (context, editableTextState) {
-        //   final TextEditingValue value = editableTextState.textEditingValue;
-        //   final List<ContextMenuButtonItem> buttonItems =
-        //       editableTextState.contextMenuButtonItems;
-
-        //   buttonItems.insert(
-        //       0,
-        //       ContextMenuButtonItem(
-        //         label: 'Reply',
-        //         onPressed: () {
-        //           widget.msgListProvider.quotedText = value.text;
-        //           widget.msgListProvider.currentParentMessageID =
-        //               widget.message.ID;
-
-        //           //messageTextFieldFocusNode.dispose();
-        //           searchFocusNode.unfocus();
-        //           //messageTextFieldFocusNode = FocusNode();
-        //           widget.messageTextFieldFocusNode.requestFocus();
-        //           widget.msgListProvider.refresh();
-        //         },
-        //       ));
-        //   return AdaptiveTextSelectionToolbar.buttonItems(
-        //     anchors: editableTextState.contextMenuAnchors,
-        //     buttonItems: buttonItems,
-        //   );
-
-        //   //     selectionControls: messageSelectionControl(msgListProvider, text,
-        //   //         message.ID, messageTextFieldFocusNode, context),
-        //   //     onSelectionChanged:
-        //   //         (TextSelection selection, SelectionChangedCause? cause) {
-        //   //   textWidgetSelection = selection;
-        //   //   isQuoteSelected.value =
-        //   //       textWidgetSelection.start != textWidgetSelection.end;
-        // },
-      );
-
-      textGestureDetectorWithMenu = GestureDetectorWithMenu(
-        isQuoteSelected: textWidgetSelection.start != 0,
-        onCopy: () {
-          //message.isSelected = false;
-          var text = message.isTaskDescriptionItem
-              ? msgListProvider.task.description
-              : message.text;
-          if (textWidgetSelection.start != textWidgetSelection.end) {
-            text = text.substring(
-                textWidgetSelection.start, textWidgetSelection.end);
-          }
-          //Pasteboard.writeText(text);
-          Clipboard.setData(ClipboardData(text: text)).then((value) {
-            //toast("Text copied to clipboard", context, 500);
-          });
-        },
+    } else if (widget.message.isTaskDescriptionItem) {
+      // Task Description
+      return ChatTextBubble(
+        text: widget.msgListProvider.task.description,
+        leading:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(
+            "Created by ${widget.msgListProvider.task.authorName} at ${dateFormat(widget.msgListProvider.task.creation_date)}",
+            style: const TextStyle(color: Colors.grey),
+          ),
+          SelectableText(
+            widget.msgListProvider.task.ID.toString().padLeft(6, '0'),
+            style: const TextStyle(color: Colors.grey),
+          ),
+        ]),
+        backgroundColor: getBubbleColor(),
+        onCopy: () => Clipboard.setData(
+            ClipboardData(text: msgListProvider.task.description)),
         onReply: () {
-          //message.isSelected = false;
-          msgListProvider.quotedText = message.isTaskDescriptionItem
-              ? msgListProvider.task.description
-              : message.text;
+          msgListProvider.quotedText = msgListProvider.task.description;
           msgListProvider.currentParentMessageID = message.ID;
-
-          msgListProvider.refresh();
-          //msgListProvider.refresh();
-          //FocusScope.of(context).unfocus();
-//          searchFocusNode.unfocus();
-          //widget.messageTextFieldFocusNode.dispose();
-          //widget.messageTextFieldFocusNode.requestFocus();
-          // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-          //   // FocusScope.of(context)
-          //   //     .requestFocus(widget.messageTextFieldFocusNode);
-          //   widget.messageTextFieldFocusNode.requestFocus();
-          // });
-        },
-        onDelete: () => msgListProvider.deleteMesage(message.ID),
-        onQuoteSelection: () async {
-          //message.isSelected = false;
-          var text = message.isTaskDescriptionItem
-              ? msgListProvider.task.description
-              : message.text;
-          text = text.substring(
-              textWidgetSelection.start, textWidgetSelection.end);
-          msgListProvider.quotedText = text;
-          msgListProvider.currentParentMessageID = message.ID;
-          //FocusScope.of(context).unfocus();
-          searchFocusNode.unfocus();
-          //messageTextFieldFocusNode.dispose();
-          //messageTextFieldFocusNode.requestFocus();
           msgListProvider.refresh();
         },
-        child: DecoratedBox(
-          // chat bubble decoration
-          decoration: BoxDecoration(
-            border: Border.all(color: const Color.fromARGB(255, 228, 232, 233)),
-            color: getBubbleColor(),
-            borderRadius: BorderRadius.circular(
-                widget.message.isTaskDescriptionItem ? 0 : 8),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            //child: IntrinsicWidth(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              if (widget.message.quotedText != null &&
-                  widget.message.quotedText!.isNotEmpty)
-                MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                        onTap: () {
-                          widget.msgListProvider
-                              .jumpTo(widget.message.parentMessageID);
-                        },
-                        child: Text(
-                          widget.message.quotedText ?? "",
-                          style: const TextStyle(color: Colors.grey),
-                        ))),
-              if (widget.message.parentsmallImageName.isNotEmpty)
-                MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: networkImage(
-                      serverURI.scheme +
-                          '://' +
-                          serverURI.authority +
-                          "/FileStorage/" +
-                          widget.message.parentsmallImageName,
-                      height: 60,
-                      headers: {"sessionID": sessionID},
-                      onTap: () {
-                        widget.msgListProvider
-                            .jumpTo(widget.message.parentMessageID);
-                      },
-                    )),
-              if (widget.message.quotedText != null &&
-                  widget.message.quotedText!.isNotEmpty)
-                const Divider(),
-              if (!widget.message.isTaskDescriptionItem &&
-                  widget.message.userName.isNotEmpty &&
-                  (widget.index == widget.msgListProvider.items.length - 1 ||
-                      widget.msgListProvider.items[widget.index + 1].userID !=
-                          widget.message.userID))
-                Text(
-                  widget.message.userName,
-                  style: const TextStyle(color: Colors.blue),
-                ),
-              //Stack(children: [
-              if (widget.message.isTaskDescriptionItem)
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Created by ${widget.msgListProvider.task.authorName} at ${dateFormat(widget.msgListProvider.task.creation_date)}",
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                      SelectableText(
-                        widget.msgListProvider.task.ID
-                            .toString()
-                            .padLeft(6, '0'),
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                    ]),
-              if (widget.message.isTaskDescriptionItem)
-                const SizedBox(height: 5),
-              textWidget,
-            ]),
-          ),
-          //  ),
-        ),
+        onQuoteSelection: (selectedText) async {
+          msgListProvider.quotedText = selectedText;
+          msgListProvider.currentParentMessageID = message.ID;
+          //searchFocusNode.unfocus();
+          msgListProvider.refresh();
+        },
       );
-      return textGestureDetectorWithMenu;
+//       final text = widget.message.isTaskDescriptionItem
+//           ? widget.msgListProvider.task.description
+//           : widget.message.text;
+//       final textSpan = TextSpan(
+//         text: text,
+//         // recognizer: TapGestureRecognizer()
+//         //   ..onSecondaryTapDown = (value) {
+//         //     print('Tap Here onTap');
+//         //   },
+//       );
+//       //BoolRef isQuoteSelected = BoolRef();
+//       final textWidget = SelectableText.rich(
+//         textSpan,
+//         onSelectionChanged: (selection, cause) {
+//           textWidgetSelection = selection;
+//           textGestureDetectorWithMenu.isQuoteSelected = true;
+//         },
+//         contextMenuBuilder: null,
+
+//         // contextMenuBuilder: (context, editableTextState) {
+//         //   final TextEditingValue value = editableTextState.textEditingValue;
+//         //   final List<ContextMenuButtonItem> buttonItems =
+//         //       editableTextState.contextMenuButtonItems;
+
+//         //   buttonItems.insert(
+//         //       0,
+//         //       ContextMenuButtonItem(
+//         //         label: 'Reply',
+//         //         onPressed: () {
+//         //           widget.msgListProvider.quotedText = value.text;
+//         //           widget.msgListProvider.currentParentMessageID =
+//         //               widget.message.ID;
+
+//         //           //messageTextFieldFocusNode.dispose();
+//         //           searchFocusNode.unfocus();
+//         //           //messageTextFieldFocusNode = FocusNode();
+//         //           widget.messageTextFieldFocusNode.requestFocus();
+//         //           widget.msgListProvider.refresh();
+//         //         },
+//         //       ));
+//         //   return AdaptiveTextSelectionToolbar.buttonItems(
+//         //     anchors: editableTextState.contextMenuAnchors,
+//         //     buttonItems: buttonItems,
+//         //   );
+
+//         //   //     selectionControls: messageSelectionControl(msgListProvider, text,
+//         //   //         message.ID, messageTextFieldFocusNode, context),
+//         //   //     onSelectionChanged:
+//         //   //         (TextSelection selection, SelectionChangedCause? cause) {
+//         //   //   textWidgetSelection = selection;
+//         //   //   isQuoteSelected.value =
+//         //   //       textWidgetSelection.start != textWidgetSelection.end;
+//         // },
+//       );
+
+//       textGestureDetectorWithMenu = GestureDetectorWithMenu(
+//         isQuoteSelected: textWidgetSelection.start != 0,
+//         onCopy: () {
+//           //message.isSelected = false;
+//           var text = message.isTaskDescriptionItem
+//               ? msgListProvider.task.description
+//               : message.text;
+//           if (textWidgetSelection.start != textWidgetSelection.end) {
+//             text = text.substring(
+//                 textWidgetSelection.start, textWidgetSelection.end);
+//           }
+//           //Pasteboard.writeText(text);
+//           Clipboard.setData(ClipboardData(text: text)).then((value) {
+//             //toast("Text copied to clipboard", context, 500);
+//           });
+//         },
+//         onReply: () {
+//           //message.isSelected = false;
+//           msgListProvider.quotedText = message.isTaskDescriptionItem
+//               ? msgListProvider.task.description
+//               : message.text;
+//           msgListProvider.currentParentMessageID = message.ID;
+
+//           msgListProvider.refresh();
+//           //msgListProvider.refresh();
+//           //FocusScope.of(context).unfocus();
+// //          searchFocusNode.unfocus();
+//           //widget.messageTextFieldFocusNode.dispose();
+//           //widget.messageTextFieldFocusNode.requestFocus();
+//           // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+//           //   // FocusScope.of(context)
+//           //   //     .requestFocus(widget.messageTextFieldFocusNode);
+//           //   widget.messageTextFieldFocusNode.requestFocus();
+//           // });
+//         },
+//         onDelete: () => msgListProvider.deleteMesage(message.ID),
+//         onQuoteSelection: () async {
+//           //message.isSelected = false;
+//           var text = message.isTaskDescriptionItem
+//               ? msgListProvider.task.description
+//               : message.text;
+//           text = text.substring(
+//               textWidgetSelection.start, textWidgetSelection.end);
+//           msgListProvider.quotedText = text;
+//           msgListProvider.currentParentMessageID = message.ID;
+//           //FocusScope.of(context).unfocus();
+//           searchFocusNode.unfocus();
+//           //messageTextFieldFocusNode.dispose();
+//           //messageTextFieldFocusNode.requestFocus();
+//           msgListProvider.refresh();
+//         },
+//         child: DecoratedBox(
+//           // chat bubble decoration
+//           decoration: BoxDecoration(
+//             border: Border.all(color: const Color.fromARGB(255, 228, 232, 233)),
+//             color: getBubbleColor(),
+//             borderRadius: BorderRadius.circular(
+//                 widget.message.isTaskDescriptionItem ? 0 : 8),
+//           ),
+//           child: Padding(
+//             padding: const EdgeInsets.all(12),
+//             //child: IntrinsicWidth(
+//             child:
+//                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//               if (widget.message.quotedText != null &&
+//                   widget.message.quotedText!.isNotEmpty)
+//                 MouseRegion(
+//                     cursor: SystemMouseCursors.click,
+//                     child: GestureDetector(
+//                         onTap: () {
+//                           widget.msgListProvider
+//                               .jumpTo(widget.message.parentMessageID);
+//                         },
+//                         child: Text(
+//                           widget.message.quotedText ?? "",
+//                           style: const TextStyle(color: Colors.grey),
+//                         ))),
+//               if (widget.message.parentsmallImageName.isNotEmpty)
+//                 MouseRegion(
+//                     cursor: SystemMouseCursors.click,
+//                     child: networkImage(
+//                       serverURI.scheme +
+//                           '://' +
+//                           serverURI.authority +
+//                           "/FileStorage/" +
+//                           widget.message.parentsmallImageName,
+//                       height: 60,
+//                       headers: {"sessionID": sessionID},
+//                       onTap: () {
+//                         widget.msgListProvider
+//                             .jumpTo(widget.message.parentMessageID);
+//                       },
+//                     )),
+//               if (widget.message.quotedText != null &&
+//                   widget.message.quotedText!.isNotEmpty)
+//                 const Divider(),
+//               if (!widget.message.isTaskDescriptionItem &&
+//                   widget.message.userName.isNotEmpty &&
+//                   (widget.index == widget.msgListProvider.items.length - 1 ||
+//                       widget.msgListProvider.items[widget.index + 1].userID !=
+//                           widget.message.userID))
+//                 Text(
+//                   widget.message.userName,
+//                   style: const TextStyle(color: Colors.blue),
+//                 ),
+//               //Stack(children: [
+//               if (widget.message.isTaskDescriptionItem)
+//                 Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: [
+//                       Text(
+//                         "Created by ${widget.msgListProvider.task.authorName} at ${dateFormat(widget.msgListProvider.task.creation_date)}",
+//                         style: const TextStyle(color: Colors.grey),
+//                       ),
+//                       SelectableText(
+//                         widget.msgListProvider.task.ID
+//                             .toString()
+//                             .padLeft(6, '0'),
+//                         style: const TextStyle(color: Colors.grey),
+//                       ),
+//                     ]),
+//               if (widget.message.isTaskDescriptionItem)
+//                 const SizedBox(height: 5),
+//               textWidget,
+//             ]),
+//           ),
+//           //  ),
+//         ),
+//       );
+//       return textGestureDetectorWithMenu;
       //);
     } else {
       // Image bubble
